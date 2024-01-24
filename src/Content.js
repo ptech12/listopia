8653abeeba6e7ae4f336cca8bb95c5bedf026adc
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-01-24 13:10:50 +0530

    minor changes

--- src/Content.js
+++ src/Content.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FaTrashAlt } from "react-icons/fa";
+import { FaTrashAlt, FaGrav, FaBox, FaDropbox } from "react-icons/fa";
 const Content = () => {
   const [items, setItems] = useState([
     {
@@ -57,34 +57,45 @@ const Content = () => {
 
   return (
     <main>
-      <ul>
-        {/* Iterate through the items using map */}
-        {
-          items.map( item => (
-            <li className="item" key={item.id}>
-              <input 
-                type="checkbox"
-                checked={item.checked}
-                onChange={() => handleCheck(item.id)} // we use onChange event for input tag
-              />
+      {
+        items.length ? (
+            <ul>
+              {/* Iterate through the items using map */}
+              {
+                items.map( item => (
+                  <li className="item" key={item.id}>
+                    <input 
+                      type="checkbox"
+                      checked={item.checked}
+                      onChange={() => handleCheck(item.id)} // we use onChange event for input tag
+                    />
 
-              <label
-                style={item.checked ? { textDecoration: 'line-through'} : null }
-                onDoubleClick={() => handleCheck(item.id)}
-              >
-                {item.item}
-              </label>
+                    <label
+                      style={item.checked ? { textDecoration: 'line-through'} : null }
+                      onDoubleClick={() => handleCheck(item.id)}
+                    >
+                      {item.item}
+                    </label>
 
-              <FaTrashAlt
-                onClick={() => handleDelete(item.id)}
-                role="button" 
-                tabIndex='0'
-              />
-            </li>
-          ))
+                    <FaTrashAlt
+                      onClick={() => handleDelete(item.id)}
+                      role="button" 
+                      tabIndex='0'
+                    />
+                  </li>
+                ))
 
+              }
+          </ul>) : (
+            <>
+              <p style={ {marginTop: '2rem'}}>
+                List is Empty
+              </p>
+              <FaDropbox />
+            </>
+
+          )
         }
-      </ul>
     </main>
   );
 };
